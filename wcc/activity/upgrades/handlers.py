fbb324c336a09ffc60d99c7210f3a58f1e37eec3
Izhar Firdaus <izhar@inigo-tech.com>
2013-01-31 11:54:49 +0800

    added upgrade profile to 1001 (re #146)

--- wcc/activity/upgrades/handlers.py
+++ wcc/activity/upgrades/handlers.py
@@ -2,3 +2,12 @@ from collective.grok import gs
 from Products.CMFCore.utils import getToolByName
 
 # -*- extra stuff goes here -*- 
+
+
+@gs.upgradestep(title=u'Upgrade wcc.activity to 1001',
+                description=u'Upgrade wcc.activity to 1001',
+                source='1', destination='1001',
+                sortkey=1, profile='wcc.activity:default')
+def to1001(context):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile('profile-wcc.activity.upgrades:to1001')
--- wcc/activity/upgrades/profiles.py
+++ wcc/activity/upgrades/profiles.py
@@ -1,3 +1,8 @@
 from collective.grok import gs
 
 # -*- extra stuff goes here -*- 
+
+gs.profile(name=u'to1001',
+           title=u'Upgrade wcc.activity to 1001',
+           description='',
+           directory='profiles/to1001')
