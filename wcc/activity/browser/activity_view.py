99aed65038143c6e2ef55a9b79b9690694e31234
glenn <glenn@glenn-Lenovo-G480>
2016-02-08 15:00:14 +0800

    sort events and filter display

--- wcc/activity/browser/activity_view.py
+++ wcc/activity/browser/activity_view.py
@@ -4,6 +4,7 @@ from wcc.activity.content.activity import IActivity
 from Products.Archetypes.interfaces.referenceable import IReferenceable
 from wcc.activity.interfaces import IActivityRelation
 grok.templatedir('templates')
+import datetime
 
 class Index(dexterity.DisplayForm):
     grok.context(IActivity)
@@ -18,7 +19,20 @@ class Index(dexterity.DisplayForm):
         return self.rels.related_news()[:3]
     
     def related_events(self):
-        return self.rels.related_events()[:3]
+        items = self.rels.related_events()
+        current = datetime.datetime.now().strftime("%Y-%m-%d %H:%m")
+        brains = []
+        objects = []
+        if items:
+            for item in items:
+                if item.start_date and item.end_date:
+                    if item.start_date.strftime("%Y-%m-%d %H:%m") >= current  or current <= item.end_date.strftime("%Y-%m-%d %H:%m"):
+                        brains.append({'start':item.start_date.strftime("%Y-%m-%d %H:%m"), 'obj':item})
+        if brains:
+            brains.sort(key=lambda val: val['start'])
+            objects = [x['obj'] for x in brains]
+        
+        return objects[:3]
 
     def related_documents(self):
         return self.rels.related_documents()[:3]
